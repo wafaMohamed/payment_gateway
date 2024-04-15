0a1d76611a9a71e1ba5c36909948976e2d85c847
wafa <wafamohameddd@gmail.com>
2024-04-15 16:27:33 +0200

    stripe model and api service

---
+++ lib/core/utils/keys.dart
@@ -0,0 +1,6 @@
+class StripeKeys {
+  static const String stripePublishableKey =
+      "pk_test_51P5TicKHEIUleMZyP9n7GyqX4nq3tUbbVlRBey2fuXE2UqLQG9BImF18MeDBw3N8GwPUbWYhacRt7PrCE4k8cOaQ00pJSTc2b3";
+  static const String secretKey =
+      "sk_test_51P5TicKHEIUleMZyYxph8hP1tlzQ871s8AwmIBOMdUvQQ8sLcpdconlVHOCYDQ86fTnbF0vhP2dQpKNHnIjfmh3x00VOIKpHDg";
+}
